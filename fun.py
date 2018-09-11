67cc65dc1f3e29369d9d0acf033e51ad73dc5d01
Jason-L <zl190@duke.edu>
2018-09-11 15:53:35 -0400

    sum and avg of three numbers

---
+++ fun.py
@@ -0,0 +1,19 @@
+def main():
+	sum_three(3, 4, 5)
+
+
+def sum_three(v1, v2, v3):
+	""" do something
+	"""
+	p = v1 + v2 + v3
+	print(p)
+
+def avg_three(v1):
+	""" do more
+	"""
+	p = v1 / 3
+	print(p)
+
+
+if __name___ == "__main__":
+	main()
