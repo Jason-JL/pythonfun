08b63e524c80d09f5d92dc5b42ad7e371e271711
Jason-L <zl190@duke.edu>
2018-09-11 16:08:16 -0400

    first code, two func: sum and avg of three numbers

--- fun.py
+++ fun.py
@@ -1,19 +1,21 @@
 def main():
 	sum_three(3, 4, 5)
-
+	avg_three(3, 4, 5)
 
 def sum_three(v1, v2, v3):
 	""" do something
 	"""
 	p = v1 + v2 + v3
-	print(p)
+	return p
 
-def avg_three(v1):
+def avg_three(v1, v2, v3):
 	""" do more
 	"""
-	p = v1 / 3
+	sum = sum_three(v1, v2, v3)
+	print(sum)
+	p =  sum / 3.0
 	print(p)
 
 
-if __name___ == "__main__":
+if __name__ == "__main__":
 	main()
